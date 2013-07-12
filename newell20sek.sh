3cf3a66282a1dd7d068c501e9a52ae0ff3c42879
ShilGen <geniusshil@gmail.com>
2013-07-13 02:17:38 +0300

    a1

--- newell20sek.sh
+++ newell20sek.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-# http://alexkutsan.blogspot.com/2012/02/linux.html
+# Copyright 2013 ShilGen.ru geniusshil@gmail.com 
+# mainstream from http://alexkutsan.blogspot.com/2012/02/linux.html
 pre="file:"
 florder="/home/gena/Изображения/best/"
 time=20
-
+#zenity --notification --window-icon /usr/share/pixmaps/bum_serv_on.png
 pic(){
   pic=`ls $florder |grep '\(png\|gif\|jpg\|jpeg\)$'|shuf -n 1`
   GSETTINGS_BACKEND=dconf gsettings set org.gnome.desktop.background picture-uri ${pre}${florder}${pic}
   sleep $time
 }
-
+yad --notification &
 i=0
 while [ ""-z ]
 do
