22f0cffe2c17bcf96e7179cd467eaafef1c66777
ShilGen <geniusshil@gmail.com>
2013-07-13 02:04:22 +0300

    breaksleep

---
+++ breaksleep.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+# Copyright 2013 ShilGen.ru geniusshil@gmail.com 
+action=$(yad --width 300 --entry --title "System Logout" \
+    --image=gnome-shutdown \
+    --button="Switch User:2" \
+    --button="gtk-ok:0" --button="gtk-close:1" \
+    --text "Choose action:" \
+    --entry-text \
+    "min10" "min30" "Cansel" "Logout")
+ret=$?
+
+[[ $ret -eq 1 ]] && exit 0
+
+if [[ $ret -eq 2 ]]; then
+    gdmflexiserver --startnew &
+    exit 0
+fi
+
+case $action in
+    min10*) cmd="sudo shutdown -P +10" ;;
+    min30*) cmd="sudo shutdown -P +30" ;;
+    Cansel*) cmd="sudo shutdown -c" ;;
+    Logout*) 
+        case $(wmctrl -m | grep Name) in
+            *Openbox) cmd="openbox --exit" ;;
+            *FVWM) cmd="FvwmCommand Quit" ;;
+            *Metacity) cmd="gnome-save-session --kill" ;; 
+            *) exit 1 ;;
+        esac
+        ;;
+    *) exit 1 ;;        
+esac
+
+eval exec $cmd
